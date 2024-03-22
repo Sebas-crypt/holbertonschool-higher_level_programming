39c8ee1f0f5254f1ee77f0d4197cb437d971430b
Sebas-crypt <usefullseba17@gmail.com>
2024-03-22 19:03:50 -0400

    putting the old code back in task 1

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,36 +1,16 @@
 #!/usr/bin/python3
+'''States filtering module'''
 import MySQLdb
-import sys
+from sys import argv
 
-def list_states_with_N(username, password, database_name):
-    # Connect to MySQL server
-    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database_name)
-
-    # Create a cursor object
-    cursor = db.cursor()
-
-    # Execute the SQL query
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-
-    # Fetch all the rows
-    results = cursor.fetchall()
-
-    # Display results
-    for row in results:
-        print(row)
-
-    # Close the cursor and database connection
-    cursor.close()
-    db.close()
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: python script.py <username> <password> <database_name>")
-        sys.exit(1)
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database_name = sys.argv[3]
-
-    list_states_with_N(username, password, database_name)
-
+    db = MySQLdb.connect(host='localhost', user=argv[1], passwd=argv[2],
+                         db=argv[3])
+    st = db.cursor()
+    st.execute("SELECT `id`, `name` FROM states WHERE name LIKE BINARY 'N%'\
+               ORDER BY `id`;")
+    res = st.fetchall()
+    for i in res:
+        print(i)
+    db.close()
